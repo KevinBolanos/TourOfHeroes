5b0d1cdb48364196690a3a3e8d549d62f29a5b80
Kevin Alarcon <kevin.alarcon@alumnos.udg.mx>
2020-05-18 18:50:51 -0500

    Tour of Heroes app Tutorial

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const app = express();
 app.use(express.static(__dirname + '/dist/angular-tour-of-heroes'));
 
 // Send all requests to index.html
-app.get('/*', function(req,res) { 
-    res.sendFile(path.join(__dirname,'/dist/angular-tour-of-heroes/index.html'));
+app.get('/*', function(req, res) { 
+    res.sendFile(path.join(__dirname + '/dist/angular-tour-of-heroes/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080)
+app.listen(process.env.PORT || 3000)
